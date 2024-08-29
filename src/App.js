55ac7deace5f7020c5ae52754114cbadc713ca3c
VolodymyrStukanov <volodymyrstukanov@gmail.com>
2024-08-29 17:38:37 +0300

    finished validating select

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import {ValidatingSelect} from "./modules/ValidatingSelect"
+import React, {useState} from "react";
 
 function App() {
+
+  var users = [{name:"Name1", id:1}, {name:"Name3", id:3}, {name:"Name2", id:2}]
+
+  const [userId1, setUserId1] = useState(undefined);
+  const [userId2, setUserId2] = useState(undefined);
+
+
+  const handleUser1SelectorChange = (e) => {
+    if (e !== undefined) {
+      setUserId1(e.id);
+    } else {
+      setUserId1(undefined);
+    }
+  }
+  const handleUser2SelectorChange = (e) => {
+    if (e !== undefined) {
+      setUserId2(e.id);
+    } else {
+      setUserId2(undefined);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div style={{margin: "2rem", maxWidth: "30%"}}>
+          <ValidatingSelect
+            required={true}
+            inputId="userSelector2"
+            getOptionLabel={option => `${option.name}`}
+            getOptionValue={option => option.id}
+            onChange={handleUser1SelectorChange}
+            options={users.length > 0 ? users : []}
+            value={users.length > 0 ? users.filter(x => x.id === userId1) : []}
+            placeholder={"Select user"}
+            invalidText={"Please choose user"}
+          />
+      </div>
+      
+      <div style={{margin: "2rem", maxWidth: "30%"}}>
+          <ValidatingSelect
+            inputId="userSelector1"
+            getOptionLabel={option => `${option.name}`}
+            getOptionValue={option => option.id}
+            onChange={handleUser2SelectorChange}
+            options={users.length > 0 ? users : []}
+            value={users.length > 0 ? users.filter(x => x.id === userId2) : []}
+            placeholder={"Select user"}
+            invalidText={"Please choose user"}
+          />
+      </div>
+
     </div>
   );
 }
---
+++ src/modules/ValidatingSelect.js
@@ -0,0 +1,90 @@
+import {useState, useEffect, useRef} from "react";
+import Select from 'react-select';
+import "../scss/Scss.scss"
+import styles from "../scss/ValidatingSelect.module.scss"
+
+
+const ValidatingSelect = ({required = false, inputId, options, value, placeholder, getOptionLabel, getOptionValue, onChange, invalidText }) => {
+
+    const [isValid, setIsValid] = useState(false);
+    const [hasBeenFocused, setHasBeenFocused] = useState(false);
+    const [isFocused, setIsFocused] = useState(false);
+    const warningRef = useRef(null);
+
+    useEffect(() => {
+        if(value !== undefined && value !== null && value.length !== undefined && value.length !== 0){
+            setIsValid(true);
+        }
+        else{
+            setIsValid(false);
+        }
+    }, [value, options]);
+
+    const handleChange = (e) => {
+        onChange(e);
+        setIsValid(true);
+    }
+
+    const handleFocus = (e) => {
+
+        warningRef.current.classList.remove("invalid-select-warning");
+        setIsFocused(true);
+        if(value !== undefined && value !== null && value.length !== undefined && value.length !== 0){
+            setIsValid(true);
+        }
+        else{
+            setIsValid(false);
+        }
+    }
+
+    const handleBlur = (e) => {
+        
+        setHasBeenFocused(true);
+        setIsFocused(false);
+    }
+
+    const warningMessage = () => {
+        return (
+            <div ref={warningRef} className={`${isValid === false && hasBeenFocused === true && isFocused === false ? "invalid-select-warning" : ""} select-warning`}>
+                <span>
+                    {invalidText}
+                </span>
+            </div>
+        )
+    }
+
+    return (
+        <>
+            <div data-isvalid={isValid}>
+                <Select
+                // className={required === true ? `select-to-valid` : ""}   //class `select-to-valid` is needed for validation
+                inputId={inputId}
+                options={options}
+                value={options !== undefined ? value : []}
+                placeholder={placeholder}
+                getOptionLabel ={getOptionLabel}
+                getOptionValue ={getOptionValue}
+                onChange={handleChange}
+                onBlur={required === true ? handleBlur : ()=>{}}
+                onFocus={required === true ? handleFocus : ()=>{}}
+                classNames={{
+                    control: (state) =>
+                    state.isFocused ? {} : isValid === true ? styles["validity-selector"] : hasBeenFocused === true ? styles["invalid-selector"] : styles["validity-selector"],
+                }}
+                styles={{
+                    menu: (props) => {
+                        return {
+                            ...props,
+                            margin: 0,
+                        };
+                    },
+                }}
+                />
+
+                {warningMessage()}
+            </div>
+        </>
+    )
+}
+
+export {ValidatingSelect};
